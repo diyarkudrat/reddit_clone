d542c3f916ea748faaa30bbe73fc0aba5b2c99d9
Diyar Kudrat <diyarkudrat@Diyars-MacBook.local>
2020-02-03 15:02:39 -0800

    users can submit and save posts

--- server.js
+++ server.js
@@ -1,20 +1,40 @@
-const express = require('express')
+const express = require('express');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator')
 
-const app = express()
+const app = express();
+
+// Use Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Adding body parser
+app.use(expressValidator());
+
+// Set db
+require('./data/reddit-db');
+
+//Controllers
+require('./controllers/posts.js')(app);
 
 
-//middleware
-const exphbs = require('express-handlebars')
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}))
-app.set('view engine', 'handlebars')
+//Middleware
+const exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 //Routes
 app.get('/', (req, res) => {
     res.render('home')
-})
+});
+
+app.get('/posts/new', (req, res) => {
+    res.render('posts-new')
+});
 
 
 app.listen(3000, () => {
     console.log('Reddit listening on localhost:3000')
-})
+});
