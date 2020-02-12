1bc0d46ad66b4856c4d548d4c84a42591b8378e3
Diyar Kudrat <diyarkudrat@Diyars-MacBook.local>
2020-02-12 13:02:10 -0800

    user can sign up

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const jwt = require('jsonwebtoken')
 
 
 module.exports = (app) => {
@@ -12,13 +13,14 @@ module.exports = (app) => {
     app.post('/sign-up', (req, res) => {
         const user = new User(req.body);
 
-        user
-            .save()
-            .then(user => {
+        user.save().then((user) => {
+                const token = jwt.sign({_id: user._id }, process.env.SECRET, { expiresIn: '60 days' });
+                res.cookie('nToken', token, {maxAge: 900000, httpOnly: true });
                 res.redirect('/');
             })
             .catch(err => {
                 console.log(err.message);
+                return res.status(400).send({ err: err });
             });
     });
 
--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
@@ -10,14 +11,30 @@ const UserSchema = new Schema({
 
 // Define the callback with a regular function to avoid problems with this
 UserSchema.pre("save", function(next) {
-    
   // SET createdAt AND updatedAt
   const now = new Date();
   this.updatedAt = now;
   if (!this.createdAt) {
     this.createdAt = now;
   }
-  next();
+  
+  // encrypt password
+  const user = this;
+  if (!user.isModified('password')) {
+    return next();
+  }
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      user.password = hash;
+      next();
+    });
+  });
 });
+
+UserSchema.methods.comparePassword = function(password, done) {
+  bcrypt.compare(password, this.password, (err, isMatch) => {
+    done(err, isMatch);
+  });
+};
 
 module.exports = mongoose.model("User", UserSchema);
