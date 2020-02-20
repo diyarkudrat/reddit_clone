e5482bb3d35a4dcea54fbfd444de9ca41174eea7
Diyar Kudrat <diyarkudrat@Diyars-MacBook.local>
2020-02-20 14:17:35 -0800

    implemented remaining authentication tests

--- test/auth.js
+++ test/auth.js
@@ -32,6 +32,30 @@ describe("User", function() {
         });
     });
 
+    //login
+    it('user should be able to login successfully', function(done) {
+        agent
+            .post('/login')
+            .send({ username: "testone", password: "password" })
+            .end(function(err, res) {
+                console.log(res.body);
+                res.should.have.status(200);
+                agent.should.have.cookie('nToken');
+                done();
+            })
+    })
+
+    //logout
+    it('user should be able to logout successfully', function(done) {
+        agent 
+            .get('/logout')
+            .end(function(err, res) {
+                res.should.have.status(200);
+                agent.should.not.have.cookie('nToken');
+                done();
+            })
+    })
+
     after(function() {
         agent.close()
     });
